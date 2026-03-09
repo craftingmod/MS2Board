e99b550be8541d1ffe1483b1d8076c322e3e18b0
craftingmod <alyac1234@gmail.com>
2026-03-09 12:55:52 +0900

    Init: Bun

---
+++ README.md
@@ -0,0 +1,15 @@
+# ms2board
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run
+```
+
+This project was created using `bun init` in bun v1.3.7. [Bun](https://bun.com) is a fast all-in-one JavaScript runtime.
---
+++ eslint.config.ts
@@ -0,0 +1,27 @@
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import { defineConfig } from "eslint/config"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+
+export default defineConfig([
+  {
+    files: [
+      "packages/*/src/**/*.{js,mjs,cjs,ts,mts,cts}",
+      "eslint.config.ts",
+      "prettier-config.ts",
+    ],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      "prefer-const": [
+        "error",
+        {
+          ignoreReadBeforeAssign: true,
+        },
+      ],
+    },
+  },
+])
